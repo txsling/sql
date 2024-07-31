66e80cd9a489df0ad9c9e0ea2bd0f6627f86bd06
Thomas Ling <thomas.ling@ek.co>
2024-07-31 16:08:36 +0100

    Add general SQL script template

---
+++ General SQL script template 2024-07-31.sql
@@ -0,0 +1,21 @@
+/* Script Title
+Create Date:        2022-02-01, last updated 2024-07-31
+Author:             Tom Ling
+Purpose: 			Short statement of why this script exists.
+Description:        Verbose description of what the query does goes here. Be specific and don't be
+                    afraid to say too much. More is better, than less, every single time. 
+					Think about "what, when, where, how and why" when authoring a description.
+Call by:            e.g. Audit Excel file
+Database:  			e.g. [databasename]
+Used By:            Operations Department
+Parameter(s):       @param1 - description and usage
+                    @param2 - description and usage
+Usage:              Additional notes or caveats about this script, like where it can and cannot 
+					be run, or gotchas to watch for when using it.
+****************************************************************************************************
+CHANGELOG
+Date(yyyy-mm-dd)    Author              Comments
+------------------- ------------------- ------------------------------------------------------------
+2022-02-01          Tom Ling			Initial script completed.
+***************************************************************************************************/
+
